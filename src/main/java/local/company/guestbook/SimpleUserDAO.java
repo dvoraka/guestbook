d6991ea3761c7b068aa90ec83afb674064e91a1b
Alen Dvorak <alen.dvorak@gmail.com>
2014-02-17 17:03:59 +0100

    Create simple DAO class

---
+++ src/main/java/local/company/guestbook/SimpleUserDAO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package local.company.guestbook;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SimpleUserDAO {
+    
+    @Autowired private SessionFactory sessionFactory;
+    
+    public long count() {
+        
+        Session session = sessionFactory.getCurrentSession();
+        List<Long> counts = session.createQuery("SELECT count(*) from User").list();
+        
+        return counts.get(0);
+        
+    } 
+}
