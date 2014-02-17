53bd982185fb4b76f176e0943771dd75c3e1e1c3
Alen Dvorak <alen.dvorak@gmail.com>
2014-02-17 22:47:48 +0100

    Add simple service layer

---
+++ src/main/java/local/company/guestbook/SimpleUserService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package local.company.guestbook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class SimpleUserService {
+    
+    @Autowired
+    private SimpleUserDAO userDAO;
+    
+    public long count() {
+        
+        return userDAO.count();
+        
+    }
+    
+}
