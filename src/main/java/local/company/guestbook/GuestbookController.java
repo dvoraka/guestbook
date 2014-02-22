84fb0cc4968d8a0a335ae71a5f4e540a94c82d08
Alen Dvorak <alen.dvorak@gmail.com>
2014-02-22 14:52:58 +0100

    Add javadocs for controller

--- src/main/java/local/company/guestbook/GuestbookController.java
+++ src/main/java/local/company/guestbook/GuestbookController.java
@@ -7,27 +7,44 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Class for guestbook controller.
+ *
+ * @author dvoraka
+ */
 @Controller
 public class GuestbookController {
 
     @Autowired
     private SimpleUserService userService;
 
+    /**
+     * Prepares index page.
+     *
+     * @param model
+     * @return
+     */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
 
         long count = userService.count();
         model.addAttribute("counter", count);
-        
+
         List<User> users = userService.getUsers();
         model.addAttribute("users", users);
 
         return "index";
     }
-    
+
+    /**
+     * Adds random user to DB and redirects to /.
+     *
+     * @param model
+     * @return
+     */
     @RequestMapping(value = "/add-rand-user/", method = RequestMethod.GET)
     public String addRandUser(Model model) {
-        
+
         return "redirect:/";
     }
 }
