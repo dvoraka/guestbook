097fec3e9aa430f547082f71da0efaee315a7c31
Alen Dvorak <alen.dvorak@gmail.com>
2016-08-27 17:02:28 +0200

    Remove unused import

--- src/main/java/local/company/guestbook/model/Comment.java
+++ src/main/java/local/company/guestbook/model/Comment.java
@@ -1,16 +1,14 @@
 package local.company.guestbook.model;
 
-import java.io.Serializable;
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "COMMENTS")
