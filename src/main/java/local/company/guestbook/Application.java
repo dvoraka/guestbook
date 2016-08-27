40a4be1dea2b3a992eefa018d81537c790d27649
Alen Dvorak <alen.dvorak@gmail.com>
2016-08-27 15:36:33 +0200

    First step of migration to Spring boot (see #3)

---
+++ src/main/java/local/company/guestbook/Application.java
@@ -0,0 +1,29 @@
+package local.company.guestbook;
+
+import org.hibernate.SessionFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+
+import javax.sql.DataSource;
+
+/**
+ * Spring boot runner.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public SessionFactory sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
+        builder.scanPackages("local.company.guestbook");
+
+        return builder.buildSessionFactory();
+    }
+
+}
