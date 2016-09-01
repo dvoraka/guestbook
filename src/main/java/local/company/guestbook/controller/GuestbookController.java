023b4037fe408370c487ea5f4c0abc930362965a
Alen Dvorak <alen.dvorak@gmail.com>
2016-09-01 12:55:46 +0200

    Update controller

--- src/main/java/local/company/guestbook/controller/GuestbookController.java
+++ src/main/java/local/company/guestbook/controller/GuestbookController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,9 +20,7 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Class for guestbook controller.
- *
- * @author dvoraka
+ * Guestbook controller.
  */
 @Controller
 public class GuestbookController {
@@ -30,11 +30,9 @@ public class GuestbookController {
     @Autowired
     private UserService userService;
 
+
     /**
      * Prepares index page.
-     *
-     * @param model
-     * @return
      */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
@@ -58,35 +56,31 @@ public class GuestbookController {
     }
 
     /**
-     * Adds random user to DB and redirects to /.
-     *
-     * @param model
-     * @return
+     * Adds a random user to the DB and redirects to /.
      */
     @RequestMapping(value = "/add-rand-user/", method = RequestMethod.GET)
-    public String addRandUser(Model model) {
+    public String addRandomUser() {
         userService.addRandomUser();
 
         return "redirect:/";
     }
 
     @RequestMapping(value = "/delete-user/{userId}", method = RequestMethod.GET)
-    public String delUser(@PathVariable("userId") long id, Model model) {
+    public String delUser(@PathVariable("userId") long id) {
         userService.deleteUser(id);
 
         return "redirect:/";
     }
 
-    @RequestMapping(value = "/reg/", method = RequestMethod.GET)
+    @GetMapping(value = "/reg/")
     public String registerForm(Model model) {
         model.addAttribute("user", new User());
 
         return "register";
     }
 
-    @RequestMapping(value = "/reg/", method = RequestMethod.POST)
-    public String registerSubmit(@Valid @ModelAttribute("user") User user, BindingResult result,
-                                 Model model) {
+    @PostMapping(value = "/reg/")
+    public String registerSubmit(@Valid @ModelAttribute("user") User user, BindingResult result) {
         if (result.hasErrors()) {
 
             return "register";
