074d592c61a377ca072212e762831775a8406311
Alen Dvorak <alen.dvorak@gmail.com>
2017-01-20 16:51:24 +0100

    Tuning

--- src/main/java/local/company/guestbook/Application.java
+++ src/main/java/local/company/guestbook/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Spring boot runner.
+ * Spring Boot runner.
  */
 @SpringBootApplication
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
