0a4585570368f72dcfca7fea1dd32cdc7f26b3e6
Alen Dvorak <alen.dvorak@gmail.com>
2017-06-12 19:35:39 +0200

    Update test

--- src/integration-test/groovy/local/company/guestbook/TestSpec.groovy
+++ src/integration-test/groovy/local/company/guestbook/TestSpec.groovy
@@ -1,14 +1,20 @@
 package local.company.guestbook
 
+import local.company.guestbook.service.AuthorService
 import local.company.guestbook.service.DefaultVoteService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
+import spock.lang.Ignore
 import spock.lang.Specification
 
 @SpringBootTest
 @ContextConfiguration
+@Ignore
 class TestSpec extends Specification {
+
+    @Autowired
+    AuthorService authorService
 
     @Autowired
     DefaultVoteService voteService
@@ -17,5 +23,15 @@ class TestSpec extends Specification {
     def "test"() {
         expect:
             println voteService.findAll()
+    }
+
+    def "generate authors"() {
+        given:
+            long count = 1_000
+
+        expect:
+            count.times {
+                authorService.addRandomAuthor()
+            }
     }
 }
