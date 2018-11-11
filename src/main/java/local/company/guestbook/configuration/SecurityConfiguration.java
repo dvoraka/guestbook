8089e528cf51278a7c2ab854b734b5580031b444
dvoraka <alen.dvorak@gmail.com>
2018-11-11 01:31:25 +0100

    #10 Improve security configuration

--- src/main/java/local/company/guestbook/configuration/SecurityConfiguration.java
+++ src/main/java/local/company/guestbook/configuration/SecurityConfiguration.java
@@ -1,34 +1,31 @@
 package local.company.guestbook.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser(User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("user")
-                        .roles("USER"));
+                .withUser("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/reg/**")
-                .hasRole("USER")
-                .antMatchers("/comment/**")
+                .antMatchers("/reg/**", "/comment/**")
                 .hasRole("USER")
                 .anyRequest()
                 .permitAll()
@@ -37,5 +34,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic()
                 .disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
