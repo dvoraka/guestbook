d5db84a34c2761c8ecccdad024e620eb42f6fa0c
dvoraka <alen.dvorak@gmail.com>
2018-11-13 01:16:24 +0100

    #24 Prepare user details service

--- src/main/java/local/company/guestbook/service/CustomUserDetailsService.java
+++ src/main/java/local/company/guestbook/service/CustomUserDetailsService.java
@@ -1,10 +1,16 @@
 package local.company.guestbook.service;
 
+import local.company.guestbook.model.Author;
 import local.company.guestbook.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -14,6 +20,15 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        Author author = authorRepository.findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new User(
+                author.getName(),
+                author.getPassword(),
+                Stream.of(new SimpleGrantedAuthority("USER"))
+                        .collect(Collectors.toList())
+        );
     }
 }
